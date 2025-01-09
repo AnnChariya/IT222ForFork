3498acb38bc4956924c76dc4c887c97bac61a5e2
Johnny <isarapong.c@rmutsvmail.com>
2025-01-09 09:24:42 +0700

    Update contact.php

--- contact.php
+++ contact.php
@@ -15,7 +15,7 @@
             <td width="190" bgcolor="#FFFF66"><ul>
 				<li><a href="home.php">หน้าแรก</li>
 				<li><a href="room.php">ห้องพัก&ราคา</li>
-				<li><a href="contact.php">ติดต่อ ต่อเติม</li>
+				<li><a href="contact.php">ติดต่อ ต่อเติม เติมต่อ</li>
 				</ul><p>
 				<?php include "login_form.php";?>
 			</td>
